9ab8635f827c5e7001e231fdc718df0680791620
Aaron Shivers <aaron@moonmail.org>
2019-01-26 21:38:34 -0600

    Updated functions to use promises and async/await

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
-const request = require('request')
+const rp = require('request-promise-native')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -20,67 +20,56 @@ app.get('/', (req, res) => {
 // Results - display search results
 app.get('/results', (req, res) => {
 
-  const geocodeAddress = (address, callback) => {
-    const addressEncoded = (encodeURIComponent(address))
+  const geocodeAddress = async (address) => {
 
-    request({
-      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressEncoded}&key=${GEOCODE_API_KEY}`,
-      json: true
-    }, (error, response, body) => {
-      if (error) {
-        callback('Unable to connect to Google servers.')
-      } else if (body.status === 'ZERO_RESULTS') {
-        callback('Unable to find that address.')
-      } else if (body.status === 'OK') {
-        callback(undefined, {
-          address: body.results[0].formatted_address,
-          latitude: body.results[0].geometry.location.lat,
-          longitude: body.results[0].geometry.location.lng
-        })
+    try {
+      const addressEncoded = (encodeURIComponent(address))
+      const options = {
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressEncoded}&key=${GEOCODE_API_KEY}`,
+        json: true
       }
-    })
-  }
-
-  const getWeather = (latitude, longitude, callback) => {
-
-    request({
-      url: `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${latitude},${longitude}`,
-      json: true
-    }, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        callback(undefined, {
-          temperature:         Math.round(body.currently.temperature),
-          feelsLike:           Math.round(body.currently.apparentTemperature),
-          temperatureHigh:     Math.round(body.daily.data[0].temperatureHigh),
-          temperatureLow:      Math.round(body.daily.data[0].temperatureLow),
-          temperatureHighTime: body.daily.data[0].temperatureHighTime,
-          currently:           body.currently.summary,
-          today:               body.daily.data[0].summary,
-          tomorrow:            body.daily.data[1].summary,
-          forecast:            body.daily.summary
-        })
-      } else {
-        callback('Unable to fetch weather.')
+      const res = await rp(options)
+      return location = {
+        address: res.results[0].formatted_address,
+        latitude: res.results[0].geometry.location.lat,
+        longitude: res.results[0].geometry.location.lng
       }
-    })
-  }
-
-  geocodeAddress(req.query.search, (error, results) => {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(results.address)
-      getWeather(results.latitude, results.longitude, (error, weatherResults) => {
-        if (error) {
-          console.log(error)
-        } else {
-          const data = (weatherResults)
-          const location = (results.address)
-          res.render('results', { pageTitle: `Weather for ${location}`, data, location })
-        }
-      })
+    } catch (error) {
+      console.error('error: ', error)
     }
-  })
+  }
+
+  const getWeather = async (latitude, longitude) => {
+
+    try {
+      const options = {
+        url: `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${latitude},${longitude}`,
+        json: true
+      }
+      const res = await rp(options)
+      return weather = {
+        temperature:         Math.round(res.currently.temperature),
+        feelsLike:           Math.round(res.currently.apparentTemperature),
+        temperatureHigh:     Math.round(res.daily.data[0].temperatureHigh),
+        temperatureLow:      Math.round(res.daily.data[0].temperatureLow),
+        temperatureHighTime: res.daily.data[0].temperatureHighTime,
+        currently:           res.currently.summary,
+        today:               res.daily.data[0].summary,
+        tomorrow:            res.daily.data[1].summary,
+        forecast:            res.daily.summary
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  geocodeAddress(req.query.search).then((location) => {
+    const address = (location.address) 
+    getWeather(location.latitude, location.longitude).then((weatherResults) => {
+      const data = (weatherResults)
+      res.render('results', { pageTitle: `Weather for ${address}`, data, address })
+    })
+  }).catch(err => console.log(err))
 })
 
 
